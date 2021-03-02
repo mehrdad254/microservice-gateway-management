04e38171c88794a92effad97b463d242d3353171
Mehrdad Asalian <lost.mhr69@gmail.com>
2021-03-02 20:45:50 +0330

    start project

---
+++ src/main/java/com/mhr/gateway/MicroserviceGatewayManagementApplication.java
@@ -0,0 +1,13 @@
+package com.mhr.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceGatewayManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceGatewayManagementApplication.class, args);
+	}
+
+}
